9986250f0741d53ce63290fe751a78ea5d4a8e4c
Achille <achille.guillon@gmail.com>
2021-10-06 12:03:05 +0200

    clean

--- strip.py
+++ strip.py
@@ -6,12 +6,8 @@ O_CLOCK = board.SCK
 O_DATA = board.MOSI
 
 class Strip:
-  def __init__(self, default_brightness=1, default_color=None, auto_write=False):
-    self.leds = adafruit_ws2801.WS2801(O_CLOCK, O_DATA, config['strip']['nb_leds'], brightness=default_brightness, auto_write=auto_write)
-
-    if default_color:
-      self.leds.fill(default_color)
-      self.leds.show()
+  def __init__(self):
+    self.leds = adafruit_ws2801.WS2801(O_CLOCK, O_DATA, config['strip']['nb_leds'], brightness=1, auto_write=False)
 
   def off(self):
     self.leds.fill(0x000000)
